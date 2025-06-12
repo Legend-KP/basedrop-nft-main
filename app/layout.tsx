72ca50c9f138719849858f3c5f53e15406c08e55
Legend-KP <kushal5paliwal@gmail.com>
2025-06-12 20:56:00 +0530

    jai ho

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-background" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
