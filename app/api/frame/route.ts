b60ab1594aa96c0286b67a04e6b79822de727729
Legend-KP <kushal5paliwal@gmail.com>
2025-06-12 12:22:49 +0530

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,1 +1,32 @@
- 
+import { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+
+export async function POST(req: NextRequest) {
+  const URL = process.env.NEXT_PUBLIC_URL;
+
+  return new Response(
+    JSON.stringify({
+      frame: {
+        version: "1",
+        image: `${URL}/basedrop-player.png`,
+        buttons: [{
+          label: "Connect Wallet",
+          action: "link",
+          target: URL
+        }],
+        aspect_ratio: "1:1"
+      }
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+}
+
+export async function GET(req: NextRequest) {
+  return new Response('Method not allowed', { status: 405 });
+} 
